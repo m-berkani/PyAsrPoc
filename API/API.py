1914ecaa8a41b2c01db6f17b3b27ed2a4a7e276c
m-berkani <m_berkani@hotmail.fr>
2023-12-08 09:33:03 +0100

    test gradio

--- API/API.py
+++ API/API.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, Form
 from aiofiles import open as async_open
 import whisper
 from whisper.tokenizer import LANGUAGES
 from typing import Optional
 import uvicorn
 import numpy as np
+import db_config as db
 
 app = FastAPI()
 
@@ -14,16 +15,25 @@ async def root():
 
 #Upload a file and return filename as reponse
 @app.post("/uploadfile")
-async def uploadfile(fileUpload: UploadFile = File(...)):
+async def uploadfile(leftContext: UploadFile = File(...),wavfile: UploadFile = File(...), ExamTypeName:str= Form(...)):
     # call whisper endpoint
-    model_size = "tiny"
+    vocabs=await db.get_Vocabs(ExamTypeName)
+    model_size = "small"
     whisperModel = whisper.load_model(model_size)
     date  = fileUpload.file.read()
     audio_int16 = np.frombuffer(date, np.int16);
     audio_float32 = int2float(audio_int16)
     result = whisperModel.transcribe(audio_float32)
+    print(result)
     return {"transcription": result["text"],
         "Status": "200 ok"}
+
+
+@app.get("/getExamType/")
+async def getExamType():
+    list_of_examTypeName= await db.get_list_examTypeName()
+    
+    return {list_of_examTypeName}
 
 def int2float(sound):
     abs_max = np.abs(sound).max()
---
+++ PyAsrPoc/class1.py
@@ -0,0 +1,67 @@
+import gradio as gr
+from transformers import pipeline
+import numpy as np
+
+
+import requests
+
+transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+
+def transcribe(stream, new_chunk):
+    sr, y = new_chunk
+    y = y.astype(np.float32)
+    y /= np.max(np.abs(y))
+
+    if stream is not None:
+        stream = np.concatenate([stream, y])
+    else:
+        stream = y
+    
+    transcription = transcriber({"sampling_rate": sr, "raw": stream})["text"]
+    return stream, transcription
+
+def save_transcription_to_file(transcription, filename="transcription.txt"):
+    with open(filename, "a") as file:
+        file.write(transcription + "\n")
+
+def save_and_transcribe(stream, new_chunk):
+    stream, transcription = transcribe(stream, new_chunk)
+    save_transcription_to_file(transcription)
+    return stream, transcription
+
+# Function to fetch a list of exams from the given URL
+def get_exam_types():
+    url = "http://65.21.200.122:30000/getExamType/"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            exams_string = response.text.strip('[]').replace(' ', '')  
+            exam_types = exams_string.split(',')
+            return exam_types
+        else:
+            print(f"Failed to fetch exam types. Status code: {response.status_code}")
+            return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+
+exam_types = get_exam_types()
+
+
+demo = gr.Interface(
+    save_and_transcribe,
+    [
+        "state",
+        gr.Audio(sources=["microphone"], streaming=True),
+        gr.Dropdown(["client 1", "client 2", "client 3"], label="Client", info="Client ID"),
+        gr.Dropdown(choices=exam_types, label="Exam Type", info="Select exam type"),
+        gr.Dropdown(["tiny", "base", "small", "medium", "large", "large-v1", "large-v2", "large-v3"], label="Model", info="Select an option"),
+        gr.File(label="Upload File", type="binary"),  # Use 'binary' instead of 'audio/*'
+        gr.Button("Submit"),
+    ],
+    ["state", "text"],
+    live=True,
+)
+
+
+demo.launch()
