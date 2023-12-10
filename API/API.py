28889aec94af42ecf7b0cb0e15f5714de30e6537
Mohamed BERKANI <m_berkani@hotmail.fr>
2023-12-10 17:46:25 +0100

    fix

--- API/API.py
+++ API/API.py
@@ -6,6 +6,7 @@ from typing import Optional
 import uvicorn
 import numpy as np
 import db_config as db
+import json
 
 app = FastAPI()
 
@@ -15,18 +16,33 @@ async def root():
 
 #Upload a file and return filename as reponse
 @app.post("/uploadfile")
-async def uploadfile(leftContext: UploadFile = File(...),wavfile: UploadFile = File(...), ExamTypeName:str= Form(...)):
+async def uploadfile(wavfile: json):
     # call whisper endpoint
-    vocabs=await db.get_Vocabs(ExamTypeName)
-    model_size = "small"
-    whisperModel = whisper.load_model(model_size)
-    date  = fileUpload.file.read()
-    audio_int16 = np.frombuffer(date, np.int16);
-    audio_float32 = int2float(audio_int16)
-    result = whisperModel.transcribe(audio_float32)
-    print(result)
-    return {"transcription": result["text"],
+    # vocabs=await db.get_Vocabs(ExamTypeName)
+    # model_size = "small"
+    # whisperModel = whisper.load_model(model_size)
+    # date  = fileUpload.file.read()
+    # audio_int16 = np.frombuffer(date, np.int16);
+    # audio_float32 = int2float(audio_int16)
+    # result = whisperModel.transcribe(audio_float32)
+    # print(result)
+    return {"transcription": "",
         "Status": "200 ok"}
+
+#Upload a file and return filename as reponse
+# @app.post("/uploadfile2")
+# async def uploadfile2(leftContext: UploadFile = File(...),wavfile: UploadFile = File(...), ExamTypeName:str= Form(...)):
+#     # call whisper endpoint
+#     vocabs=await db.get_Vocabs(ExamTypeName)
+#     model_size = "small"
+#     whisperModel = whisper.load_model(model_size)
+#     date  = fileUpload.file.read()
+#     audio_int16 = np.frombuffer(date, np.int16);
+#     audio_float32 = int2float(audio_int16)
+#     result = whisperModel.transcribe(audio_float32)
+#     print(result)
+#     return {"transcription": result["text"],
+#         "Status": "200 ok"}
 
 
 @app.get("/getExamType/")
@@ -44,4 +60,5 @@ def int2float(sound):
     return sound
 
 if __name__ == "__main__":
+    
     uvicorn.run(app, host="0.0.0.0", port=30000)
--- PyAsrPoc/class2.py
+++ PyAsrPoc/class2.py
@@ -1,21 +1,66 @@
 import gradio as gr
 from transformers import pipeline
 import numpy as np
+import webrtcvad
+import torch
+import json
+import requests
+#transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
 
-transcriber = pipeline("automatic-speech-recognition", model="openai/whisper-base.en")
+def int2float(sound):
+    abs_max = np.abs(sound).max()
+    sound = sound.astype('float32')
+    if abs_max > 0:
+        sound *= 1/32768
+    sound = sound.squeeze()  # depends on the use case
+    return sound
 
 def transcribe(stream, new_chunk):
+    global file_count
+    global text
     sr, y = new_chunk
-    y = y.astype(np.float32)
-    y /= np.max(np.abs(y))
+    #y = y.astype(np.float32)
+    #y /= np.max(np.abs(y))
 
-    if stream is not None:
-        stream = np.concatenate([stream, y])
+    audio_float32 = int2float(y)
+    confidence = model(torch.from_numpy(audio_float32), sr).item()
+    # if(is_speech):
+    #     text1 = 'yes'
+    # else : 
+    #     text1 = 'no'
+    if confidence> 0.5:
+        if stream is not None :
+            stream = np.concatenate([stream, audio_float32])
+        else:
+            stream = audio_float32
     else:
-        stream = y
-    return stream, transcriber({"sampling_rate": sr, "raw": stream})["text"]
+        if (stream is not None):
+            tensor = torch.tensor(stream )
+            save_audio(str(file_count)+".wav",tensor,sr)
+            transcribe_api(stream)
+            stream=None
+            file_count=file_count+1
+            
+    return stream, confidence#8T8T8952({"sampling_rate": sr, "raw": stream})["text"]°3 06 °8790I?V  B.GLIHDVVSQVBNL/NTNG?NXS?CS.S.S.
+
+def transcribe_api(stream):
+    url = "http://localhost:30000/uploadfile"
+    
+    # Convert NumPy array to a list (JSON serializable)
+    array_as_list = stream.tolist()
+
+    # Convert the list to a JSON string
+    json_data = json.dumps({"data": array_as_list})
+    response = requests.post(url, json=json_data)
+    return response.json()
 
 
+
+model, utils = torch.hub.load(
+        repo_or_dir="snakers4/silero-vad", model="silero_vad", onnx=False)
+(get_speech_timestamps, save_audio, read_audio, VADIterator, collect_chunks) = utils
+file_count = 1
+text =""
 demo = gr.Interface(
     transcribe,
     ["state", gr.Audio(sources=["microphone"], streaming=True)],
